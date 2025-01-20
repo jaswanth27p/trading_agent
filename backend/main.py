32728174ea510c1f48d67c82668db4f3f08295ba
jaswanth27p <jaswanth27p@gmail.com>
2025-01-20 21:59:24 +0530

    trying to integrate gemini-2.0-flash llm

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from google.
+import os
 
 app = FastAPI()
+
+g.generativeai.configure(api_key=os.environ[GOOGLE_API_KEY])
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,3 +22,9 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/gemini")
+async def gemini():
+    model = genai.GenerativeModel("gemini-2.0-flash-exp")
+    response = model.generate_content("How does AI work?")
+    return response.text
